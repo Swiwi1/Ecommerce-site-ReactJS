2b6fe2e2bc3300bce44e11b4c6fdc19ff7adbd6c
Swiwi1 <lfclose123@gmail.com>
2024-06-13 17:31:43 +0100

    Signup + button component completed

--- ecommerce site/src/routes/sign-in/sign-in.jsx
+++ ecommerce site/src/routes/sign-in/sign-in.jsx
@@ -1,16 +1,32 @@
+
+
 import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { 
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth, 
+} from '../../utils/firebase/firebase.utils'
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form'
 
 
 const SignIn = () => {
+  
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     const userDocRef = await createUserDocumentFromAuth(user)
   }
+
+    // const logGoogleRedirectUser = async () => {
+    //   const { user } = await signInWithGoogleRedirect()
+    //   console.log({user})
+    // }
+
   return (
     <div>
       <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign up with google</button>
+      <button onClick={logGoogleUser}>Sign up with Google popup</button>
+      <SignUpForm/>
       </div>
   )
 }
